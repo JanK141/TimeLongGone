933d9d792283827f15419354a41af89ae47d5fb2
s18835 <60381611+s18835@users.noreply.github.com>
2022-03-19 16:13:10 +0100

    Update PlayerInput.cs
    add debug.logs

--- Time Long Gone/Assets/Content/Scripts/Player/PlayerInput.cs
+++ Time Long Gone/Assets/Content/Scripts/Player/PlayerInput.cs
@@ -14,54 +14,78 @@ namespace Content.Scripts.Player
             var keyboard = Keyboard.current;
 
             if (keyboard.wKey.isPressed)
+            {
                 playerScript.isUpPressed = true;
+                Debug.Log("wantMoveUp");
+            }
+
             if (keyboard.sKey.isPressed)
+            {
                 playerScript.isDownPressed = true;
+                Debug.Log("wantMoveDown");
+            }
+
             if (keyboard.aKey.isPressed)
+            {
                 playerScript.isLeftPressed = true;
+                Debug.Log("wantMoveLeft");
+            }
+
             if (keyboard.dKey.isPressed)
+            {
                 playerScript.isRightPressed = true;
+                Debug.Log("wantMoveRight");
+            }
         }
 
         public void WantDash(InputAction.CallbackContext context)
         {
-            if (context.performed)
-                playerScript.isDashPressed = true;
+            if (!context.performed) return;
+            playerScript.isDashPressed = true;
+            Debug.Log("wantDash");
         }
 
         public void WantJump(InputAction.CallbackContext context)
         {
-            if (context.performed)
-                playerScript.isJumpPressed = true;
+            if (!context.performed) return;
+            playerScript.isJumpPressed = true;
+            Debug.Log("wantJump");
         }
 
         public void WantAttack(InputAction.CallbackContext context)
         {
-            if (context.performed)
-                playerScript.isAttackPressed = true;
+            if (!context.performed) return;
+            playerScript.isAttackPressed = true;
+            Debug.Log("wantAttack");
         }
 
         public void WantChargeAttack(InputAction.CallbackContext context)
         {
-            if (context.performed)
-                playerScript.isChargeAttackPressed = true;
+            if (!context.performed) return;
+            playerScript.isChargeAttackPressed = true;
+            Debug.Log("wantChargeAttack");
         }
 
         public void WantBlock(InputAction.CallbackContext context)
         {
-            if (context.performed)
-                playerScript.isBlockPressed = true;
+            if (!context.performed) return;
+            playerScript.isBlockPressed = true;
+            Debug.Log("wantBlock");
         }
 
         public void WantTime(InputAction.CallbackContext context)
         {
-            if (context.performed)
-                playerScript.isTimePressed = true;
+            if (!context.performed) return;
+            playerScript.isTimePressed = true;
+            Debug.Log("wantTime");
         }
 
 
         public void WantStun(InputAction.CallbackContext context)
-            =>
-                playerScript.CurrentCombat = PlayerScript.CombatStatus.Stun;
+        {
+            if (!context.performed) return;
+            playerScript.CurrentCombat = PlayerScript.CombatStatus.Stun;
+            Debug.Log("wantStun");
+        }
     }
 }
